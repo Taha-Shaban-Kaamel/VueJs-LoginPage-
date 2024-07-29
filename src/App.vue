a82792bd5e5ba2f173b8db68d03777f1140e34cd
Taha-Shaban-Kaamel <tahashaban743@gmail.com>
2024-07-29 03:49:49 +0300

    sigin with google fututre

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <div id="app">
+<GoogleLogin 
+      :client-id="CLIENT_ID"
+      :scope="scope"
+      :callback="callback" 
+      prompt 
+      auto-login
+    />
     <LoginForm />
   </div>
 </template>
 
-<script>
+<script setup>
 import LoginForm from './components/LoginForm.vue'
-
-export default {
-  components: { LoginForm }
+const CLIENT_ID = "750236939237-rhaii2cs23jgmmqsp487hq47mv6nre5i.apps.googleusercontent.com"
+const scope = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
+ const callback = (response) => {
+     if (response.error) {
+    console.error(response.error);
+  } else {
+    const { accessToken, profileObj } = response;
+    console.log(accessToken, profileObj);
+    // Authenticate the user and authorize access to your application
 }
+  }
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100vh;
-}
 
-#app {
-  font-family: "Poppins", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  overflow: hidden;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+  };
+
+  #app {
+    font-family: "Poppins", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    overflow: hidden;
+  }; 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import vue3GoogleLogin from 'vue3-google-login'
+const CLIENT_ID = "750236939237-rhaii2cs23jgmmqsp487hq47mv6nre5i.apps.googleusercontent.com"
+createApp(App)
+    .use(vue3GoogleLogin, {
+        clientId:CLIENT_ID
+    })
+    .mount('#app')
